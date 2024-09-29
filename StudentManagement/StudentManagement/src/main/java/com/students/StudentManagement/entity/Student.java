aa6e5c64b5bc0bee3b45c0cbec9a72c72a84b94a
SIRAMRULWA <49866831+SIRAMRULWA@users.noreply.github.com>
2024-09-29 21:04:44 +0200

    Add files via upload
    Student management SpringBoot

---
+++ StudentManagement/StudentManagement/src/main/java/com/students/StudentManagement/entity/Student.java
@@ -0,0 +1,21 @@
+package com.students.StudentManagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
